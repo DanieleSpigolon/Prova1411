2f343c37b685987eefddc8e3392814e331fbb59d
DanieleSpigolon <72398566+DanieleSpigolon@users.noreply.github.com>
2020-11-14 16:43:46 +0100

    Aggiunti test per MenuItem

---
+++ src/test/java/it/unipd/tos/model/MenuItemTest.java
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////
+// [Prova] [Prova] [1100]
+////////////////////////////////////////////////////////////////////
+package it.unipd.tos.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import it.unipd.tos.model.ItemType;
+import it.unipd.tos.model.MenuItem;
+
+public class MenuItemTest {
+
+    private static MenuItem menuItem;
+
+    @BeforeClass
+    public static void beforeClass() {
+        menuItem = new MenuItem(ItemType.Fritti, "Olive Ascolane", 4);
+    }
+
+    @Test
+    public void testGetItemType() {
+        assertEquals(ItemType.Fritti, menuItem.getItemType());
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("Olive Ascolane", menuItem.getName());
+    }
+
+    @Test
+    public void testGetPrice() {
+        assertEquals(4, menuItem.getPrice(), 0);
+    }
+
+}
